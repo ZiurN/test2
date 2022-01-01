1b95955d1dee7327c0f5e0a554e8fcc73493ab61
jeferson ruiz <jeferson.ruiz@globant.com>
2022-01-01 16:25:15 -0300

    SF-383
    https://avalian.atlassian.net/browse/SF-383
    
    feature/SF-383-errorSegmentosEventosMedicos
    
    Se crea el componente LWC segmentoResponseListener para poder escuchar el
    evento enviado al recibir una respuesta fallida de SS al actualizar o crear
    el segemento allí.
    
    Este componente simplemente notifica por un pop-up al usuario del error y
    actualiza la vista del registro para tratar de mostrar el campo de error
    diligenciado automaticamente, sin tener que recargar la página.
    
    Se agrega dicho componente a la flexipage Segmento_P_gina_de_registro1.
    
    Se realizan además algunos otros ajustes a las clases de apex involucradas
    para ajustar el comportamiento a lo esperado en el front.

--- force-app/main/default/aura/eventoMedicoEnviarASSBtn/eventoMedicoEnviarASSBtnHelper.js
+++ force-app/main/default/aura/eventoMedicoEnviarASSBtn/eventoMedicoEnviarASSBtnHelper.js
@@ -1,16 +1,15 @@
 ({
 	goToApex: function (component, event) {
-	    let id = component.get('v.recordId');
+		let id = component.get('v.recordId');
 		let helper = this;
 		component.set('v.isLoading', true);
-
 		LightningUtils.callApex(
 			component,
 			"sendEventoMedicoToSS",
 			function(succeed, result, errors) {
 				if(succeed) {
 					if(!result.hasError) {
-						if(result.message == 'ok') {
+						if(result.message == 'OK') {
 							LightningUtils.showToast(
 								'Evento Médico enviado',
 								'El evento médico ha sido enviado satisfactoriamente',
@@ -22,7 +21,7 @@
 						$A.get('e.force:refreshView').fire();
 					}
 					else {
-                        LightningUtils.showToast("Error", result.message, {"type":"error"});
+						LightningUtils.showToast("Error", result.message, {"type":"error"});
 					}
 				} else {
 					helper.showError(errors);
@@ -34,68 +33,57 @@
 			}
 		);
 	},
-
 	showMissingFields : function (component, event) {
-	    let message = '';
-	    let stop = false;
-
-        message += this.showRelationshipFieldMessage(component, 'Codigo_de_Afiliado__c', 'Asociado', 'Asociado__');
-        message += this.showRelationshipFieldMessage(component, 'Codigo_del_Diagnostico__c', 'Diagnóstico', 'Diagnostico__');
-        message += this.showFieldMessage(component, 'Tipo__c', 'Tipo');
-        message += this.showRelationshipFieldMessage(component, 'Codigo_de_efector__c', 'Efector Solicitante', 'Efector_Solicitante__');
-        message += this.showRelationshipFieldMessage(component, 'Codigo_de_delegacion__c', 'Delegación', 'Delegacion__');
-        message += this.showFieldMessage(component, 'Fecha_y_Hora_inicio_EMS__c', 'Fecha y hora inicio EM');
-        message += this.showFieldMessage(component, 'Sin_complicaciones__c', 'Sin complicaciones');
-        if(message) {
-            LightningUtils.showToast(
-                "Campos incompletos",
-                message
-            );
-            stop = true;
-            message = '';
-        }
-        return stop;
-    },
-
-    showFieldMessage : function (component, fieldApi, fieldName) {
-        let msg = '';
-        if(!component.get('v.eventoMedicoSimpleRecord')[fieldApi]) {
-            msg += '- ' + fieldName + ': debe estar completo \n';
-        }
-        return msg;
-    },
-
-    showRelationshipFieldMessage : function (component, fieldApi, fieldName, relationship) {
-        let msg = '';
-        if(!component.get('v.eventoMedicoSimpleRecord')[relationship + 'c']
-                || (component.get('v.eventoMedicoSimpleRecord')[relationship + 'c']
-                    && !component.get('v.eventoMedicoSimpleRecord')[relationship + 'r'][fieldApi])
-            ) {
-            msg += '- ' + fieldName + ': debe estar completo \n';
-        }
-        return msg;
-    },
-
+		let message = '';
+		let stop = false;
+		message += this.showRelationshipFieldMessage(component, 'Codigo_de_Afiliado__c', 'Asociado', 'Asociado__');
+		message += this.showRelationshipFieldMessage(component, 'Codigo_del_Diagnostico__c', 'Diagnóstico', 'Diagnostico__');
+		message += this.showFieldMessage(component, 'Tipo__c', 'Tipo');
+		message += this.showRelationshipFieldMessage(component, 'Codigo_de_efector__c', 'Efector Solicitante', 'Efector_Solicitante__');
+		message += this.showRelationshipFieldMessage(component, 'Codigo_de_delegacion__c', 'Delegación', 'Delegacion__');
+		message += this.showFieldMessage(component, 'Fecha_y_Hora_inicio_EMS__c', 'Fecha y hora inicio EM');
+		message += this.showFieldMessage(component, 'Sin_complicaciones__c', 'Sin complicaciones');
+		if(message) {
+			LightningUtils.showToast(
+				"Campos incompletos",
+				message
+			);
+			stop = true;
+			message = '';
+		}
+		return stop;
+	},
+	showFieldMessage : function (component, fieldApi, fieldName) {
+		let msg = '';
+		if(!component.get('v.eventoMedicoSimpleRecord')[fieldApi]) {
+			msg += '- ' + fieldName + ': debe estar completo \n';
+		}
+		return msg;
+	},
+	showRelationshipFieldMessage : function (component, fieldApi, fieldName, relationship) {
+		let msg = '';
+		if(!component.get('v.eventoMedicoSimpleRecord')[relationship + 'c']
+				|| (component.get('v.eventoMedicoSimpleRecord')[relationship + 'c']
+					&& !component.get('v.eventoMedicoSimpleRecord')[relationship + 'r'][fieldApi])
+			) {
+			msg += '- ' + fieldName + ': debe estar completo \n';
+		}
+		return msg;
+	},
 	showError: function(errors) {
 		let errorMsg = '';
-    
 		if(errors[0].message) {
 			errorMsg = errors[0].message;
-		} 
+		}
 		else {
-        
 			for(const key in errors[0].fieldErrors) {
 				errorMsg += `${errors[0].fieldErrors[key][0].message}.`;
 			}
-
 			errorMsg += errors[0].pageErrors && errors[0].pageErrors.length ? errors[0].pageErrors[0].message : '';
-
 			if(!errorMsg) {
 				errorMsg = 'Contacte con un administrador';
 			}
 		}
-
 		LightningUtils.showToast("Error", errorMsg , {"type":"error"});
 	}
-
 })
