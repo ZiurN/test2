93a6d5352e4bc559f9fc1b40515fd8ac3a84b82d
jeferson ruiz <jeferson.ruiz@globant.com>
2022-01-19 17:27:58 -0300

    SF-38
    https://avalian.atlassian.net/browse/SF-38
    
    Modificaciones al Perfil Disca
    
    Se refactoriza el llamado para revertir los casos a SS, para centrar la
    lógica en la clase WSCaseEnviarASSHelper, y el parseo en la clase
    WSSSGenericResponse.
    
    A partir de esto, se cambian las dependencias a las clases WSCaseAPRevertir,
    WSCaseAPRevertirResp, WSCaseAPRevertirRespTest y WSCaseAPRevertirTest,
    pasando los métodos de prueba de esta última clase a la clase
    WSCaseEnviarASSHelperTest.
    
    Estas clases se agregan al paquete destructivo para ser borradas.
    
    Se remaqueta ligeramente el componente aura caseAPRevertirBtn.
    
    Dado que la respuesta esperada en el caso de revertir no devuelve una lista
    en la propiedad AP, se tiene que refactorizar la clase WSSSGenericResponse
    para sortear esta particularidad.
    
    Se crea la metadata para el endpoint centro_autorizador_revertir.
    
    Finalmente se agregan los Labels creados.

--- force-app/main/default/aura/caseAPRevertirBtn/caseAPRevertirBtnHelper.js
+++ force-app/main/default/aura/caseAPRevertirBtn/caseAPRevertirBtnHelper.js
@@ -2,23 +2,22 @@
 	goToApex: function (component, event, caseIdExterno) {
 		let helper = this;
 		component.set('v.isLoading', true);
-
 		LightningUtils.callApex(
 			component,
 			"deleteAlertasRelatedToCaseAndChangeStatusFromCase",
 			function(succeed, result, errors) {
 				if(succeed) {
 					if(!result.hasError) {
-					    if(result.message == 'ok') {
-                            LightningUtils.showToast(
-                                "Caso Revertido",
-                                'El caso ha sido revertido correctamente',
-                                 {"type":"success"}
-                             );
+						if(result.message == 'ok') {
+							LightningUtils.showToast(
+								"Caso Revertido",
+								'El caso ha sido revertido correctamente',
+								 {"type":"success"}
+							 );
 							 $A.get('e.force:refreshView').fire();
-                        } else {
-						    LightningUtils.showToast("No Se Pudo Revertir", result.message);
-                        }
+						} else {
+							LightningUtils.showToast("No Se Pudo Revertir", result.message);
+						}
 					}
 					else {
 						LightningUtils.showToast("Error", result.message, {"type":"error"});
@@ -33,58 +32,44 @@
 			}
 		);
 	},
-
 	showError: function(errors) {
 		let errorMsg = '';
-
 		for(const key in errors[0].fieldErrors) {
 			errorMsg += `${errors[0].fieldErrors[key][0].message};`;
-
 		}
 		errorMsg += errors[0].pageErrors.length ? errors[0].pageErrors[0].message : '' ;
-
 		if(!errorMsg) {
 			errorMsg = 'Contacte con un administrador';
 		}
-
 		LightningUtils.showToast("Error", errorMsg , {"type":"error"});
 	},
-
 	isLeader: function (component) {
-		
 		return new Promise(
 		  $A.getCallback(function(resolve, reject) {
-			let action = component.get("c.isLeader"); 
-			
+			let action = component.get("c.isLeader");
 			action.setCallback(this, function(response) {
 				if(response.getState() === 'SUCCESS') {
 					component.set('v.isLeader', response.getReturnValue());
 					component.set('v.isLeaderVarSetted', true);
-
 					resolve(response.getReturnValue());
 				}
 				else {
 					resolve(false);
 				}
-				
 			});
 			$A.enqueueAction(action);
 		  })
 		);
-		
 	},
-
-	ableDisableButton: function(component) {
+	toggleEnableButton: function(component) {
 		let button = component.find('disableRevertirAPId');
 		let estado = component.get('v.caseSimpleRecord').Status;
 		let rechazado = component.get('v.caseSimpleRecord').Caso_Rechazado__c || estado == 'Rechazado';
 		let isLeader = component.get('v.isLeader');
-
 		if (estado == 'CA-----G' || estado == 'CA-----E' || estado == 'CA-----N' || (rechazado && !isLeader)) {
 			button.set('v.disabled', true);
 		} else {
 			button.set('v.disabled', false);
 		}
-
 	}
 })
